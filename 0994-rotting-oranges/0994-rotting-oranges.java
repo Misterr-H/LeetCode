36efeb7b8485a95a063c6cb38434962fb64577a1
Himanshu Saini <55010619+Misterr-H@users.noreply.github.com>
2025-02-06 20:38:44 +0530

    Time: 3 ms (14.54%), Space: 42.5 MB (61.21%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,69 +1,61 @@
 class Info {
     int x;
     int y;
-
-    public Info(int x, int y) {
+    int duration;
+    public Info(int x, int y, int duration) {
         this.x = x;
         this.y = y;
+        this.duration = duration;
     }
 }
 
 class Solution {
     public int orangesRotting(int[][] grid) {
+        int ans = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+
         Queue<Info> q = new LinkedList<>();
-        boolean rotten = false;
-        boolean fresh = false;
-        for(int i = 0; i < grid.length; i++) {
-            for( int j = 0; j < grid[0].length; j++) {
+        int[][] visited = new int[m][n];
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
                 if(grid[i][j] == 2) {
-                    q.add(new Info(i,j));
-                    rotten = true;
-                }
-                if(grid[i][j] == 1){
-                    fresh = true;
+                    q.offer(new Info(i, j, 0));
+                    visited[i][j] = 2;
+                } else if(grid[i][j] == 1) {
+                    visited[i][j] = 1;
                 }
             }
         }
-        if(fresh == false) {
-            return 0;
-        }
-        if(rotten == false) {
-            return -1;
-        }
-        int min = 0;
+
+        int[] drow = {0, -1, 0, +1};
+        int[] dcol = {-1, 0, 1, 0};
+
         while(!q.isEmpty()) {
-            int n = q.size();
-            while(n>0) {
-                Info info = q.poll();
-                int r = info.x;
-                int c = info.y;
-                if(r+1<grid.length && grid[r+1][c]==1){
-                    q.add(new Info(r+1,c));
-                    grid[r+1][c] = 2;
-                }
-                if(r-1 >= 0 && grid[r-1][c] == 1) {
-                    q.add(new Info(r-1, c));
-                    grid[r-1][c] = 2;
-                }
-                if(c+1 < grid[0].length && grid[r][c+1] == 1) {
-                    q.add(new Info(r, c+1));
-                    grid[r][c+1] = 2;
-                }
-                if(c-1 >= 0 && grid[r][c-1] == 1) {
-                    q.add(new Info(r, c-1));
-                    grid[r][c-1] = 2;
-                }
-                n--;
-            }
-            min++;
-        }
-        for(int i = 0; i < grid.length; i++) {
-            for(int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1) {
-                    return -1;
+            Info curr = q.poll();
+            int r = curr.x;
+            int c = curr.y;
+            int t = curr.duration;
+            ans = Math.max(ans, t);
+            for(int i = 0; i<4; i++) {
+                int nrow = r + drow[i];
+                int ncol = c + dcol[i];
+
+                if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && grid[nrow][ncol] == 1 && visited[nrow][ncol] != 2) {
+                    q.offer(new Info(nrow, ncol, t+1));
+                    visited[nrow][ncol] = 2;
                 }
             }
+
         }
-        return min - 1;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(visited[i][j] == 1) return -1;
+            }
+        }
+
+        return ans;
     }
 }
