3831d8e6980ad809bbe605d63baa613815eebbdc
Himanshu Saini <55010619+Misterr-H@users.noreply.github.com>
2025-02-08 14:04:00 +0530

    Time: 48 ms (29.12%), Space: 50.2 MB (26.92%) - LeetHub

--- 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.java
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.java
@@ -1,3 +1,5 @@
+// RECURSIVE DP
+
 // class Solution {
 //     long[][][] dp;
 //     int count = 0;
@@ -32,29 +34,61 @@
 //             take = solve(s, index+1, selections-1, s.charAt(index) - '0');
 //         } else {
 //             // dp[index][selections][prev+1] = solve(s, index+1, selections, prev);
-            
-            
+
 //         }
 //         skip = solve(s, index+1, selections, prev);
 //         return dp[index][selections][prev+1] = take+skip;
 //     }
 // }
 
+// ITERATIVE DP
+
+// class Solution {
+//     public long numberOfWays(String s) {
+//         int n = s.length();
+//         long[][] dp = new long[4][2]; // Only store for last char: 0 or 1
+
+//         dp[0][0] = dp[0][1] = 1; // Base case: No selections means 1 way
+
+//         for (char c : s.toCharArray()) {
+//             int num = c - '0';
+
+//             for (int i = 3; i > 0; i--) { // Reverse iteration to prevent overwriting
+//                 dp[i][num] += dp[i - 1][1 - num]; // Transition step
+//             }
+//         }
+//         return dp[3][0] + dp[3][1]; // Sum of all valid sequences of length 3
+//     }
+// }
+
+// SOLUTION USING PREFIX SUM
+// IDEA is to calculate prefix sum of 0's and 1's, then fix middle element and count pnc
 
 class Solution {
     public long numberOfWays(String s) {
         int n = s.length();
-        long[][] dp = new long[4][2]; // Only store for last char: 0 or 1
+        long[] prez = new long[n];
+        long[] preo = new long[n];
+        long ans = 0;
 
-        dp[0][0] = dp[0][1] = 1; // Base case: No selections means 1 way
-        
-        for (char c : s.toCharArray()) {
-            int num = c - '0';
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                prez[i] = s.charAt(i) == '0' ? 1 : 0;
+                preo[i] = s.charAt(i) == '1' ? 1 : 0;
+            } else {
+                prez[i] = prez[i - 1] + ((s.charAt(i) == '0') ? 1 : 0);
+                preo[i] = preo[i - 1] + ((s.charAt(i) == '1') ? 1 : 0);
+            }
 
-            for (int i = 3; i > 0; i--) { // Reverse iteration to prevent overwriting
-                dp[i][num] += dp[i - 1][1 - num]; // Transition step
+        }
+
+        for (int i = 1; i < n - 1; i++) {
+            if (s.charAt(i) == '0') {
+                ans += preo[i] * (preo[n - 1] - preo[i]);
+            } else {
+                ans += prez[i] * (prez[n - 1] - prez[i]);
             }
         }
-        return dp[3][0] + dp[3][1]; // Sum of all valid sequences of length 3
+        return ans;
     }
 }
